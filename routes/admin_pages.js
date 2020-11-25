5f4aff403c4386717c852602c955418da01b2662
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2020-11-25 06:55:45 -0800

    Add files via upload

--- routes/admin_pages.js
+++ routes/admin_pages.js
@@ -1,9 +1,26 @@
 const express=require('express');
 const router=express.Router();
+// get pages index 
 
 router.get('/',(req,res,next) =>{
     res.send('admin pages!');
 });
+
+// get add page
+
+router.get('/add-page',(req,res,next) =>{
+    const title=" ";
+    const slug=" ";
+    const content=" ";
+
+    res.render('admin/add_page',{
+        title:title,
+        slug:slug,
+        content:content
+
+    });
+});
+
 
 
 module.exports=router;
