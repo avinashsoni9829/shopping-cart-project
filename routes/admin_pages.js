8089c4da89274aa808d8bd07ac8d7b21a0c55eb8
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2020-11-28 22:20:14 -0800

    Add files via upload

--- routes/admin_pages.js
+++ routes/admin_pages.js
@@ -9,9 +9,9 @@ router.get('/',(req,res,next) =>{
 // get add page
 
 router.get('/add-page',(req,res,next) =>{
-    const title=" ";
-    const slug=" ";
-    const content=" ";
+    var title=" ";
+    var slug=" ";
+    var content=" ";
 
     res.render('admin/add_page',{
         title:title,
@@ -19,6 +19,37 @@ router.get('/add-page',(req,res,next) =>{
         content:content
 
     });
+});
+
+
+
+// POST add page
+
+router.post('/add-page',(req,res,next) =>{
+    req.checkBody('title','title must have a value').notEmpty();
+    req.checkBody('content','Content  must have a value').notEmpty();
+    var title=req.body.title;
+    var slug=req.body.slug.replace(/\s+/g,'-').toLowerCase();
+    if(slug=="")
+    slug=title.replace(/\s+/g,'-').toLowerCase();
+    var content=req.body.content;
+    
+    var errors=req.validationErrors();
+    if(errors)
+    {
+        res.render('../views/admin/add_page',{
+            errors:errors,
+            title: title,
+            slug:slug,
+            content:content
+        });
+    }
+    else{
+         console.log("success!");
+
+    }
+
+    
 });
 
 
